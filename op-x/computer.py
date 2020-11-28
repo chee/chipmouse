5104f6fd23730e0fab6b401c144420df151f4132
chee <chee@snoot.club>
2020-11-28 21:45:59 +0000

    Delete computer.py
    only computers can use Computers:(

--- op-x/computer.py
+++
@@ -1,9 +0,0 @@
-from .mode import Mode
-from tkinter import Tk
-
-class Computer():
-	def __init__(self, mode):
-		if mode is not Mode.COMPUTER:
-			raise RuntimeError("only computers can use Computers")
-
-		self.root = Tk()
