4d4ef17c21d8393e844f1f6c340f38c5d5168ec7
chee <chee@snoot.club>
2020-11-29 10:27:16 +0000

    Remove unused `view.py` file
    EVERYTHING IS A MENU now

--- op-x/view.py
+++
@@ -1,7 +0,0 @@
-from .screen import Screen
-from .controls import Controls
-
-class View():
-        name = "abstract view"
-        def start(self, screen: Screen, controls: Controls):
-                raise Exception("not implemented")
