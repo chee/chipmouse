d741de281447836f2f425cce106b1454790ef07b
chee <chee@snoot.club>
2020-12-01 17:59:24 +0000

    add halt command

--- op-x/menus/main.py
+++ op-x/menus/main.py
@@ -1,5 +1,16 @@
+from ..controls import Control
 from typing import List
 from ..menu import Menu, Option, MenuWithSubmenus
+from os import system
 
 class MainMenu(MenuWithSubmenus):
         name = "main"
+        quit_counter = 0
+        def handle_control_up(self, control):
+                if control is not Control.menu_exit:
+                        self.quit_counter = 0
+        def quit():
+                super().quit()
+                quit_counter += 1
+                if quit_counter == 4:
+                        system("halt")
