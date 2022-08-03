8f0d54598e6c6da4d110922a643cea2425f27fa3
chee <yay@chee.party>
2022-08-03 10:26:20 -0500

    update url & email address

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setuptools.setup(
 	name="chipmouse-chee",
 	version="0.0.0",
 	author="chee",
-	author_email="chee@snoot.club",
+	author_email="yay@chee.party",
 	description="a companion to the op-1",
 	long_description=long_description,
 	long_description_content_type="text/org",
-	url="https://git.snoot.club/chee/chipmouse",
+	url="https://github.com/chee/chipmouse",
 	packages=setuptools.find_packages(),
 	python_requires=">= 3.6",
 	entry_points={
